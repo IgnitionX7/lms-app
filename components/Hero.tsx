94460cbe16d8d0481ab7e6a5c8bfa55f8475d6de
Shan Amin <robin_zill@yahoo.com>
2025-03-19 22:20:50 +0500

    Creating the Courses Grid and updated Hero banner

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function Hero() {
   return (
     <div className="relative h-[400px] w-full">
       {/* Background Image */}
-      <img
+      <Image
         src="/hero-banner.jpg"
         alt="Background"
-        className="h-full w-full object-cover"
+        fill
+        priority
+        className="object-cover"
       />
 
       {/* Dark Overlay */}
@@ -19,11 +24,15 @@ function Hero() {
         <p className="text-lg text-white/80 mt-2">
           Your path to mastering skills.
         </p>
-        <button className="mt-4 bg-primary text-white dark:text-black px-4 py-2 rounded-full">
+        <Link
+          href="/courses"
+          className="mt-4 bg-primary text-white dark:text-black px-4 py-2 rounded-full transition-all duration-300 ease-in-out hover:translate-y-[-4px]"
+        >
           Enroll Now
-        </button>
+        </Link>
       </div>
     </div>
   );
 }
+
 export default Hero;
--- sanity/lib/student/getStudentByClerkId.ts
+++ sanity/lib/student/getStudentByClerkId.ts
@@ -3,6 +3,13 @@ import { sanityFetch } from "../live";
 
 export async function getStudentByClerkId(clerkId: string) {
   const getStudentByClerkIdQuery = defineQuery(
-    `*[_type=="student" && clerkId== $clerkId][0]`
+    `*[_type == "student" && clerkId == $clerkId][0]`
   );
+
+  const student = await sanityFetch({
+    query: getStudentByClerkIdQuery,
+    params: { clerkId },
+  });
+
+  return student.data;
 }
